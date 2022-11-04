551635926a4d0d5bd89b918e52974720aa5d624c
eLilyPad <elizabethbryce00@gmail.com>
2022-11-04 12:18:03 +0000

    init structure of code

---
+++ scrabble/src/game.py
@@ -0,0 +1,37 @@
+class Tile:
+    def points():
+        pass
+
+class Word:
+    def verify():
+        pass
+        
+    def points():
+        pass
+    
+class Bag:
+    def give_tiles():
+        pass
+    
+class Player:
+    def longest_word():
+        pass
+
+    def best_word():
+        pass
+
+    def best_triple_word():
+        pass
+
+def get_dictionary():
+    pass
+
+def user_input():
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
