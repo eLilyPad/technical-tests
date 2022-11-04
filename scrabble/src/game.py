e5eecefdd3fc8f37d837f3f0f75bed0aa2f20ce6
eLilyPad <elizabethbryce00@gmail.com>
2022-11-04 12:31:57 +0000

    get points of letters

--- scrabble/src/game.py
+++ scrabble/src/game.py
@@ -1,8 +1,24 @@
 class Tile:
-    def points():
-        pass
+    _points_charts = {
+        1: ['E', 'A', 'I', 'O', 'N', 'R', 'T', 'L', 'S', 'U'],
+        2: ['D', 'G'],
+        3: ['B', 'C', 'M', 'P'],
+        4: ['F', 'H','V','W','Y'],
+        5: ['K'],
+        8: ['J','X'],
+        10: ['Q','Z'],
+    }
+    
+    def points(letter):
+        for score, letters in Tile._points_charts.items():
+            if letter in letters:
+                return score 
+            
+        return 0 #TODO: Invalid Letter
 
 class Word:
+    
+    
     def verify():
         pass
         
@@ -10,6 +26,18 @@ class Word:
         pass
     
 class Bag:
+    
+    _distribution = {
+        27: ['E'],
+        20: ['A','I'],
+        18: ['O'],
+        13: ['N','R','T'],
+        9: ['L','S','U','D'],
+        7: ['G'],
+        4: ['B','C','M','P','F','H','V','W','Y'],
+        2: ['K','J','X','Q','Z'],
+    }
+    
     def give_tiles():
         pass
     
